43f2306df77007499eaef5395efeff67cdc05440
Alyssa Mell <ajmell7@uw.edu>
2026-02-27 10:54:14 -0800

    Made required changes to existing plots and begin testing with gif page

--- helpers/plot_helpers.py
+++ helpers/plot_helpers.py
@@ -6,6 +6,8 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import plotly.express as px
 import plotly.graph_objects as go
+import contextily as cx
+import io
 
 def nautical_miles_to_meters(nautical_miles):
     """
@@ -38,6 +40,8 @@ def get_lightining_groups(bin_times, bin_starts, bin_ends, best_track_df, glm_df
     # Get lightning group counts for each bin of histogram
     lightning_groups_inner_core = []
     lightning_groups_outer_core = []
+    lightning_groups_all = []
+    glm_df = glm_df[glm_df["Group Quality Flag"] == 0] # filter to only include quality flag 0
 
     for idx in range(len(bin_times)):
         bin_time = bin_times[idx]
@@ -51,6 +55,8 @@ def get_lightining_groups(bin_times, bin_starts, bin_ends, best_track_df, glm_df
             rmw_meters = nautical_miles_to_meters(rmw_nautical)
             inner_core_filter = 1.5*rmw_meters
             outer_core_filter = 5*rmw_meters
+            # get land vs ocean at bin time 
+            physical_geography = best_track_df[best_track_df['Timestamp'] == bin_time]['Geography'].values[0]
             # filter bin time
             filter_glm_data = glm_df[(glm_df["Group Time"] >= bin_start) & (glm_df["Group Time"] < bin_end)]
             # filter data to only include lightning within 400 km of hurricane center
@@ -58,30 +64,38 @@ def get_lightining_groups(bin_times, bin_starts, bin_ends, best_track_df, glm_df
             filter_glm_data_outer_core = filter_glm_data[(filter_glm_data["Distance From Hurricane Center (m)"] > inner_core_filter) & (filter_glm_data["Distance From Hurricane Center (m)"] <= outer_core_filter)]
             num_groups_inner_core = len(filter_glm_data_inner_core)
             num_groups_outer_core = len(filter_glm_data_outer_core)
+            num_groups_all = num_groups_inner_core + num_groups_outer_core
             #print(num_groups)
-            lightning_groups_inner_core.append([bin_time,num_groups_inner_core])
-            lightning_groups_outer_core.append([bin_time,num_groups_outer_core])
+            lightning_groups_inner_core.append([bin_time,num_groups_inner_core, physical_geography])
+            lightning_groups_outer_core.append([bin_time,num_groups_outer_core, physical_geography])
+            lightning_groups_all.append([bin_time,num_groups_all, physical_geography])
         except:
             print('No Lightning')
 
-    lightning_groups_inner_core_df = pd.DataFrame(lightning_groups_inner_core, columns=["time", "groups"])
-    lightning_groups_outer_core_df = pd.DataFrame(lightning_groups_outer_core, columns=["time", "groups"])
-    return lightning_groups_inner_core_df, lightning_groups_outer_core_df
+    lightning_groups_inner_core_df = pd.DataFrame(lightning_groups_inner_core, columns=["time", "groups", "geography"])
+    lightning_groups_outer_core_df = pd.DataFrame(lightning_groups_outer_core, columns=["time", "groups", "geography"])
+    lightning_groups_all_df = pd.DataFrame(lightning_groups_all, columns=["time", "groups", "geography"])
+    return lightning_groups_inner_core_df, lightning_groups_outer_core_df, lightning_groups_all_df
 
 
 
 def create_histogram(lightning_groups_inner_core_df,lightning_groups_outer_core_df,best_track_df,hurricane_name,hurricane_year):
     fig, axes = plt.subplots(1, 2, figsize=(16, 6), sharey=False)
-    fig.suptitle(f"{hurricane_name} {hurricane_year}: GLM Groups", fontsize=16)
+    fig.suptitle(f"{hurricane_name} {hurricane_year}: GLM Groups \n (blue = water, orange = land)", fontsize=16)
 
     # Helper to draw one panel
     def plot_panel(ax, lightning_df, title):
+        geo_colors = {
+            "Ocean": "tab:blue",
+            "Land": "tab:orange"
+        }
+        geo_colors = lightning_df["geography"].map(geo_colors)
         # Left axis: GLM group count
         ax.bar(
             lightning_df['time'],
             lightning_df['groups'],
             width=0.03,
-            color='tab:blue',
+            color= geo_colors, #'tab:blue',
             label='GLM Groups'
         )
         ax.set_title(title)
@@ -179,30 +193,81 @@ def plot_hurricane_path(best_track_df, hurricane):
 
     return fig
 
+def calculate_hurricane_category(wind_speed_series):
+    bins = [-1, 33, 63, 82, 95, 112, 136, float("inf")]
+    labels = [
+        "Tropical Depression",
+        "Tropical Storm",
+        "Category 1",
+        "Category 2",
+        "Category 3",
+        "Category 4",
+        "Category 5",
+    ]
+    return pd.cut(wind_speed_series, bins=bins, labels=labels)
+
 
 def plot_hurricane_path_interactive(best_track_df, hurricane):
+    best_track_df['Category'] = calculate_hurricane_category(best_track_df['Maximum Sustained Winds'])
     fig = px.scatter_mapbox(
         best_track_df,
         lat="Latitude",
         lon="Longitude",
+        color = "Category",
         hover_data={
             "Timestamp": True,
+            "Latitude": ':.4f',
+            "Longitude": ':.4f',
             "Minimum Pressure": True,
             "Maximum Sustained Winds": True,
-            "Radius of Maximum Winds": True,
-            "Latitude": False,
-            "Longitude": False
+            "Radius of Maximum Winds": True
+        },
+        labels={
+            "Minimum Pressure": "Minimum Pressure (mb)",
+            "Maximum Sustained Winds": "Maximum Sustained Winds (knots)",
+            "Radius of Maximum Winds": "Radius of Maximum Winds (nautical miles)"
         },
         zoom=3,
         height=800,
         width = 600
     )
 
-    fig.update_traces(mode="lines+markers")
-
-
     fig.update_layout(
         mapbox_style="open-street-map"
     )
 
     return fig
+
+
+def plot_bin_lightning(final_df, bin_start, bin_end, center_lat, center_lon, box_size):
+    '''
+    Plot lightning groups in vicinity of hurricane center
+
+    Args:
+        final_df: Dataframe containing lightning group data within time bin
+        bin_start: Start datetime of bin
+        bin_end: End datetime of bin
+        center_lat: Latitude of hurricane center to draw on plot
+        center_lon: Longitude of hurricane center to draw on plot
+        box_size: Size of box around hurricane center to show in plot
+
+    Returns:
+        None (just shows plot)
+    '''
+    fig, ax = plt.subplots(figsize=(8,8))
+    plt.scatter(final_df['long'], final_df['lat'], s=2, label='Lightning Group')
+    plt.scatter(center_lon, center_lat, color='black', label='Hurricane Center')
+    plt.xlim(center_lon - box_size, center_lon + box_size)
+    plt.ylim(center_lat - box_size, center_lat + box_size)
+    plt.xlabel('Longitude (deg)')
+    plt.ylabel('Latitude (deg)')
+    cx.add_basemap(ax, crs="EPSG:4326", source=cx.providers.OpenStreetMap.Mapnik)
+    plt.title(f'Lightning Groups Near Hurricane Ian Center, {bin_start}-{bin_end}')
+    plt.legend()
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png", dpi=150, bbox_inches="tight")
+    plt.close(fig)
+    buf.seek(0)
+
+    return buf
